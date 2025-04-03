8c4bf14d4c9e5b92c526bf0d5fb8efc734931d34
Prashant kumar <114512068+Prashant-kr01@users.noreply.github.com>
2025-04-03 23:20:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 function toCase(text) {
   // write your code here
-}
+	return text.toLowerCase() + '-' + text.toUpperCase();
 
+}
+  
 // DO not change the code below
 
 const text = prompt("Enter text:");
